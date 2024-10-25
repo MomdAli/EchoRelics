df0bce58697ecfd6d18cf59b17ffd059aab4c648
Marcel77718 <marcel.friesen101@gmx.de>
2024-10-25 13:13:59 +0200

    test coverage raised

--- echo-relics/src/main/scala/Main.scala
+++ echo-relics/src/main/scala/Main.scala
@@ -1,14 +1,15 @@
 import scala.io.AnsiColor._
-import com.echorelics.core.{Grid, GameManager}
-import com.echorelics.math.Position
-import com.echorelics.model.Player
+
+import de.htwg.se.echorelics.core.GameManager
+import de.htwg.se.echorelics.core.Grid
+import de.htwg.se.echorelics.model.Player
 
 @main def echorelics(): Unit = {
   println(s"\n${GREEN}Welcome to Echo Relics${RESET}\n")
 
   // Setup the grid and the player
   val grid = Grid.defaultGrid()
-  val player = Player.defaultPlayer(grid)
+  val player = Player.defaultPlayer()
 
   // Initialize the game controller and start the game
   val gameManager = new GameManager(player, grid)
--- echo-relics/src/main/scala/com/echorelics/model/Echo.scala
+++
@@ -1,9 +0,0 @@
-package com.echorelics.model
-
-import com.echorelics.math.Position
-
-final case class Echo(playerID: String, position: Position) {
-  def replayAction(): Unit = {
-    println(s"Echo of player $playerID replays action at $position")
-  }
-}
