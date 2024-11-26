15b829d0d3af0028b61be122965d7893caaaccfe
MomdAli <mohammadali.alsaiaf@gmail.com>
2024-11-26 22:27:33 +0100

    fixed small bug

--- src/test/scala/de/htwg/se/echorelics/model/EchoSpec.scala
+++ src/test/scala/de/htwg/se/echorelics/model/EchoSpec.scala
@@ -2,52 +2,49 @@ package model
 
 import org.scalatest.matchers.should.Matchers
 import org.scalatest.wordspec.AnyWordSpec
-import model.events.{EventManager, GameEvent, EventListener}
+import model.events.{EventManager, GameEvent}
+import utils.Position
+import model.events.EventListener
 
 class EchoSpec extends AnyWordSpec with Matchers {
 
   "An Echo" should {
-    val player = Player("1")
-    val echo = Echo("1", Player("1"))
+
     "be created with a given id and owner" in {
+      val player = Player("1")
+      val echo = Echo("echo1", player)
       echo.id should be("echo1")
       echo.owner should be(player)
     }
 
-    "subscribe to the EventManager on creation" in {
+    "subscribe to EventManager on creation" in {
+      val player = Player("1")
+      val echo = Echo("echo1", player)
+      EventManager.isSubscribed(echo) should be(true)
+    }
+
+    "handle OnPlayerMoveEvent and notify OnMoveEchoEvent" in {
+      val player = Player("1")
+      val echo = Echo("echo1", player)
       var eventNotified = false
       val listener = new EventListener {
         override def handleEvent(event: GameEvent): Unit = {
           event match {
-            case GameEvent.OnMoveEchoEvent(_) => eventNotified = true
-            case _                            => // do nothing
+            case GameEvent.OnMoveEchoEvent(e) if e == echo =>
+              eventNotified = true
+            case _ => // do nothing
           }
         }
       }
       EventManager.subscribe(listener)
-
       EventManager.notify(GameEvent.OnPlayerMoveEvent)
       eventNotified should be(true)
     }
 
-    "notify OnMoveEchoEvent when a player moves" in {
-      var eventNotified = false
-      val listener = new EventListener {
-        override def handleEvent(event: GameEvent): Unit = {
-          event match {
-            case GameEvent.OnMoveEchoEvent(_) => eventNotified = true
-            case _                            => // do nothing
-          }
-        }
-      }
-      EventManager.subscribe(listener)
-
-      EventManager.notify(GameEvent.OnPlayerMoveEvent)
-      eventNotified should be(true)
-    }
-
-    "print a message when the owner player dies" in {
-
+    "handle OnPlayerDeathEvent and print message" in {
+      val player = Player("1")
+      val echo = Echo("echo1", player)
+      // Redirect console output to capture the print statement
       val stream = new java.io.ByteArrayOutputStream()
       Console.withOut(stream) {
         EventManager.notify(GameEvent.OnPlayerDeathEvent(player))
